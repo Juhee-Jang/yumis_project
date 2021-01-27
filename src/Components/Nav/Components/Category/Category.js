0480c81231713aad92bab280a911b4bb7881c149
Juhee Jang <juheejang.dev@gmail.com>
2021-01-27 18:51:43 +0900

    Add: 카테고리 컴포넌트 완성

---
+++ src/Components/Nav/Components/Category/Category.js
@@ -0,0 +1,35 @@
+import React from "react";
+import CategoryIcon from "../../../../Image/Nav/categoryIcon.svg";
+import styled from "styled-components";
+
+function Category() {
+  return (
+    <WrapCategory>
+      <Link>
+        <Icon src={CategoryIcon} alt="categoryIcon" />
+        카테고리
+      </Link>
+    </WrapCategory>
+  );
+}
+
+export default Category;
+
+const WrapCategory = styled.div`
+  height: 69px;
+  font-size: 20px;
+  font-weight: 500;
+  border-top: 1px solid rgb(221, 221, 221);
+`;
+
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 20px 230px;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+`;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Category from "./Components/Category/Category";
+import styled from "styled-components";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <WrapNav>
+      <Category />
+    </WrapNav>
+  );
 }
 
 export default Nav;
+
+const WrapNav = styled.div`
+  height: 266px;
+  padding-top: 58px;
+  border-bottom: 1px solid ${(props) => props.theme.borderColor};
+`;
