963cd0978976de1bee62ac6b13e47b64bbb3cd18
Juhee Jang <juheejang.dev@gmail.com>
2021-01-27 18:05:41 +0900

    초기세팅

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
 import GlobalStyle from "./style/global";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
-    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Routes />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/style/theme.js
@@ -0,0 +1,5 @@
+const theme = {
+  borderColor: "rgb(221, 221, 221)",
+};
+
+export default theme;
