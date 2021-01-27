c6308898307150dcc7af6bdc4940d27e9fd7d0f3
Juhee Jang <juheejang.dev@gmail.com>
2021-01-27 21:59:12 +0900

    Add: 푸터 회사정보 컴포넌트 완료

---
+++ src/Components/Footer/Components/CompanyInfo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function CompanyInfo() {
+  return <div></div>;
+}
+
+export default CompanyInfo;
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,21 @@
 import React from "react";
+import CompanyInfo from "./Components/CompanyInfo";
+import HomePageInfo from "./Components/HomePageInfo";
+import styled from "styled-components";
 
 function footer() {
-  return <div>footer</div>;
+  return (
+    <WrapFooter>
+      <CompanyInfo />
+      <HomePageInfo />
+    </WrapFooter>
+  );
 }
 
 export default footer;
+
+const WrapFooter = styled.div`
+  display: flex;
+  padding: 40px 180px 94px 233px;
+  border-top: 1px solid ${(props) => props.theme.borderColor};
+`;
