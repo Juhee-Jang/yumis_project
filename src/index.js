30e506bb0e26eff53830e0119b4bcd7e502d16d0
Juhee Jang <juheejang.dev@gmail.com>
2021-01-27 17:50:16 +0900

    초기세팅

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import GlobalStyle from "./style/global";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes />
+    <GlobalStyle />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
